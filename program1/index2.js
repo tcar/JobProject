84cc6fa3de7048bea191f2d2254be1ba0e357a81
Tihomir Car <tcar121293@gmail.com>
2017-10-02 21:48:50 +0200

    test send

---
+++ program1/index2.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize')
+const nats = require('nats').connect();
+
+nats.on('connect',(nats)=>{
+   let i =0
+    setInterval( function (){
+        i++
+        const data =  JSON.stringify({
+            id: i,
+            content_uid: 'bla'
+          });
+        
+          nats.publish('foo', data, function() {
+        
+        });
+    },3000)
+
+})
+
+   
+
+    
+    
+
+  
+  
+
+
+
---
+++ program2/index.js
@@ -0,0 +1,24 @@
+const nats = require('nats').connect();
+
+nats.on('connect',(nats)=>{
+   let i =0
+    setInterval( function (){
+        i++
+        const data =  JSON.stringify({
+            id: i,
+            content_uid: 'bla'
+          });
+          nats.publish('foo', data, function() {
+        
+        });
+    },3000)
+
+})
+
+   
+
+    
+    
+
+  
+  
